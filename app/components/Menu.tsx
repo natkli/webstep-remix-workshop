c894448fa678d39f4f59fcd9a46a5a66b22bd0e7
natkli <nattap1901@hotmail.com>
2023-02-13 02:10:20 +0100

    feat: Added active menu element highlighter

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,5 +1,6 @@
-import { Link } from "@remix-run/react";
-import { type ReactElement } from "react";
+import { Link, useLocation } from "@remix-run/react";
+import clsx from "clsx";
+import { useState, type ReactElement } from "react";
 import { TbFlame, TbHome2, TbUser } from "react-icons/tb";
 
 interface IMenuList {
@@ -9,29 +10,34 @@ interface IMenuList {
 
 const menuList: IMenuList[] = [
   {
-    link: "",
+    link: "/",
     icon: <TbHome2 size={24} />,
   },
   {
-    link: "ranking",
+    link: "/ranking",
     icon: <TbFlame size={24} />,
   },
   {
-    link: "profile",
+    link: "/profile",
     icon: <TbUser size={24} />,
   },
 ];
 
 export default function Menu() {
+  const { pathname } = useLocation();
+
   return (
     <div className="nav fixed bottom-0 z-40 w-full max-w-[32rem] bg-primary text-primary-content">
       <ul className={`grid h-[4em] grid-cols-3`}>
-        {menuList.map(({ link, icon }, index) => {
+        {menuList.map(({ link, icon }) => {
           return (
             <Link
-              key={index}
+              key={link}
               to={link}
-              className="flex items-center justify-center"
+              className={clsx(
+                "flex items-center justify-center border-b-2",
+                link === pathname ? "border-icing-red" : "border-primary"
+              )}
             >
               <li>{icon}</li>
             </Link>
