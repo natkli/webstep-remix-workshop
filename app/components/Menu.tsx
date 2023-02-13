b4500e16a396e82bbfda33d40bff3147ee96919f
natkli <nattap1901@hotmail.com>
2023-02-13 01:48:38 +0100

    feate: Added Husky and bump deps

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@remix-run/react";
-import { ReactElement } from "react";
+import { type ReactElement } from "react";
 import { TbFlame, TbHome2, TbUser } from "react-icons/tb";
 
 interface IMenuList {
